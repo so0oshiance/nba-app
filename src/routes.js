8b0d800af7bb131ab37e08225c3af27da8890d55
so0oshiance <m.farhani@rightel.ir>
2019-08-02 18:18:52 +0430

    setup app

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import {Route,Switch} from 'react-router-dom'
+
+
+export default class routes extends Component {
+    render() {
+        return (
+            <Switch>
+                
+            </Switch>
+        )
+    }
+}
